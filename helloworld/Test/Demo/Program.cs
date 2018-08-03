bbb76ec9677fa069181b8e351ef260e57e236cb7
Anh Tuan <anhtuannd188@gmail.com>
2018-08-03 23:37:16 +0700

    Update welcome message

--- helloworld/Test/Demo/Program.cs
+++ helloworld/Test/Demo/Program.cs
@@ -10,7 +10,7 @@ namespace Demo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world");
+            Console.WriteLine("Hello world Anh Tuan");
 
             Console.ReadLine();
         }
